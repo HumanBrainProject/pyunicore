a50b72687bbcfce0b6a32429d954680e42620a5d
Mike Gevaert <michael.gevaert@epfl.ch>
2017-06-14 14:42:30 +0200

    added better downloading, rename, copy of files
    * update decorator to become like @property

--- pyunicore/client.py
+++ pyunicore/client.py
@@ -1,21 +1,36 @@
-'''Client for Unicore'''
-#TODO:
-# - Add site picking
-# - Add Application discovery and launch creation
+'''Client for Unicore
 
-import cStringIO
+Based on https://sourceforge.net/p/unicore/wiki/REST_API/
+'''
+
+# TODO:
+# - Add Application discovery and launch creation
+#   >>> BSP = client.get_application('BSP')
+#   >>> BSP('foo', 'bar', 'baz')  # to launch it
+
+# - Turn off verify=False once certificates are correct
+
 import logging
 import os
 import re
 import time
+import sys
 
 from contextlib import closing
 from datetime import datetime, timedelta
 
 import requests
 
+
+if sys.version_info < (3, 0):
+    from types import StringType
+else:
+    StringType = str  # pragma: no cover
+
 L = logging.getLogger(__name__)
+
 requests.packages.urllib3.disable_warnings()
+
 _REST_CACHE_TIMEOUT = 5  # in seconds
 _HBP_REGISTRY_URL = ('https://hbp-unic.fz-juelich.de:7112'
                      '/HBP/rest/registries/default_registry')
@@ -31,6 +46,7 @@ _FACTORY_RE = re.compile(_FACTORY_RE, re.VERBOSE)
 
 
 def get_sites(transport, registry_url=_HBP_REGISTRY_URL):
+    '''Get all sites from registery'''
     resp = transport.get(url=registry_url)
     site_urls = (prop['href']
                  for prop in resp['entries']
@@ -40,30 +56,36 @@ def get_sites(transport, registry_url=_HBP_REGISTRY_URL):
     return sites
 
 
-class TimedCache(object):
-    '''decorator so that calls are only made once per `timeout`'''
+class TimedCacheProperty(object):
+    '''decorator to create get only property; values are fetched once per `timeout`'''
     def __init__(self, timeout):
         self._timeout = timedelta(seconds=timeout)
-        self._last_lookup = datetime.min
-        self._value = None
+        self._func = None
+        self._values = {}
+
+    def __get__(self, instance, cls):
+        last_lookup, value = self._values.get(instance, (datetime.min, None))
+        now = datetime.now()
+        if self._timeout < now - last_lookup:
+            value = self._func(instance)
+            self._values[instance] = now, value
+        return value
 
     def __call__(self, func):
-        def wrap(*args, **kwargs):
-            now = datetime.now()
-            if self._timeout < now - self._last_lookup:
-                self._last_lookup = now
-                self._value = func(*args, **kwargs)
-            return self._value
-        return wrap
+        self._func = func
+        return self
 
 
 class Transport(object):
     '''wrapper around requests to add authentication headers'''
-    def __init__(self, oidc_token):
+    def __init__(self, oidc_token, verify=False):
         super(Transport, self).__init__()
         self.oidc_token = oidc_token
+        # TODO: should default to True once certificates are correct
+        self.verify = verify
 
     def _headers(self, kwargs):
+        '''required headers for REST calls'''
         headers = {'Authorization': 'Bearer %s' % self.oidc_token,
                    'Accept': 'application/json',
                    }
@@ -75,22 +97,29 @@ class Transport(object):
         return headers
 
     def get(self, to_json=True, **kwargs):
+        '''do get
+
+        Note:
+            For the raw response, set `to_json` to false
+        '''
         headers = self._headers(kwargs)
-        req = requests.get(headers=headers, verify=False, **kwargs)
+        req = requests.get(headers=headers, verify=self.verify, **kwargs)
         req.raise_for_status()
         if not to_json:
             return req
         return req.json()
 
     def put(self, **kwargs):
+        '''do put'''
         headers = self._headers(kwargs)
-        req = requests.put(headers=headers, verify=False, **kwargs)
+        req = requests.put(headers=headers, verify=self.verify, **kwargs)
         req.raise_for_status()
         return req
 
     def post(self, **kwargs):
+        '''do post'''
         headers = self._headers(kwargs)
-        req = requests.post(headers=headers, verify=False, **kwargs)
+        req = requests.post(headers=headers, verify=self.verify, **kwargs)
         req.raise_for_status()
         return req
 
@@ -98,15 +127,16 @@ class Transport(object):
 class Client(object):
     '''Entrypoint to Unicore API
 
-        >>> base_url = '...'
         >>> token = '...'
         >>> transport = Transport(token)
-        >>> client = Client(transport, base_url)
+        >>> sites = get_sites(transport)
+        >>> client = Client(transport, sites['HBP_JULIA'])
         >>> # to get the jobs
-        >>> client.get_jobs()
+        >>> jobs = client.get_jobs()
         >>> # to start a new job:
-        >>> client.new_job(...)
-
+        >>> launch_params = {'Executable': cmd,
+        >>> }
+        >>> job = client.new_job(launch_params)
     '''
     def __init__(self, transport, site_url):
         super(Client, self).__init__()
@@ -117,24 +147,16 @@ class Client(object):
             for k, v in self.transport.get(url=site_url)['_links'].items()}
 
     def get_jobs(self):
+        '''return a list of `Job` objects'''
         return [Job(self.transport, url)
                 for url in self.transport.get(url=self.site_urls['jobs'])['jobs']]
 
-    def new_job(self, cmd):
-        launch_params = {'Executable': cmd,
-                         }
+    def new_job(self, launch_params):
+        '''create a new job by launching `cmd`'''
         resp = self.transport.post(url=self.site_urls['jobs'],
                                    json=launch_params)
         job_url = resp.headers['Location']
         return Job(self.transport, job_url)
-
-
-class Application(object):
-    '''wrapper around Unicore job'''
-    def __init__(self, transport, app_url):
-        super(Job, self).__init__()
-        self.transport = transport
-        self.app_url = app_url
 
 
 class Job(object):
@@ -144,18 +166,18 @@ class Job(object):
         self.transport = transport
         self.job_url = job_url
 
-    @property
-    @TimedCache(timeout=_REST_CACHE_TIMEOUT)
+    @TimedCacheProperty(timeout=_REST_CACHE_TIMEOUT)
     def properties(self):
+        '''properties of the job'''
         return self.transport.get(url=self.job_url)
 
-    @property
-    @TimedCache(timeout=3600)
+    @TimedCacheProperty(timeout=3600)
     def working_dir(self):
         '''return the working directory'''
         return PathDir(
             self.transport,
-            self.properties['_links']['workingDirectory']['href'])
+            self.properties['_links']['workingDirectory']['href'],
+            '/')
 
     def is_running(self):
         '''checks whether a job is still running'''
@@ -163,14 +185,17 @@ class Job(object):
         return status not in ('SUCCESSFUL', 'FAILED', )
 
     def abort(self):
+        '''abort the job'''
         url = self.properties['_links']['action:abort']['href']
         self.transport.post(url=url, json={})
 
     def restart(self):
+        '''restart the job'''
         url = self.properties['_links']['action:restart']['href']
         self.transport.post(url=url, json={})
 
     def start(self):
+        '''start the job'''
         url = self.properties['_links']['action:start']['href']
         self.transport.post(url=url, json={})
 
@@ -186,16 +211,21 @@ class Job(object):
             time.sleep(_REST_CACHE_TIMEOUT + 0.1)
 
     def __repr__(self):
-        return 'Job: %s: running: %s' % (self.job_id, self.is_running())
+        return ('Job: %s: %s, submitted: %s running: %s' %
+                (self.job_id,
+                 os.path.basename(self.properties['_links']['self']['href']),
+                 self.properties['submissionTime'],
+                 self.is_running()))
 
     __str__ = __repr__
 
 
 class Path(object):
-    def __init__(self, transport, path_url):
+    def __init__(self, transport, path_url, name):
         super(Path, self).__init__()
         self.transport = transport
         self.path_url = path_url
+        self.name = name
 
     @property
     def path_urls(self):
@@ -210,25 +240,20 @@ class Path(object):
         '''is a file'''
         return False
 
-    #def get_metadata(self, name):
-    #    pass
-    #def remove(self, name):
-    #    '''remove file'''
-    #    pass
-    #def rename(self, name):
-    #    '''rename file'''
-    #    pass
+    def __repr__(self):
+        return '%s: %s' % (self.__class__.__name__, self.name)
+
+    __str__ = __repr__
 
 
 class PathDir(Path):
-    def __init__(self, transport, path_url):
-        super(PathDir, self).__init__(transport, path_url)
+    def __init__(self, transport, path_url, name):
+        super(PathDir, self).__init__(transport, path_url, name)
 
         self._last_contents_lookup = datetime.min
         self._cached_contents = {}
 
-    @property
-    @TimedCache(timeout=_REST_CACHE_TIMEOUT)
+    @TimedCacheProperty(timeout=_REST_CACHE_TIMEOUT)
     def contents(self):
         return self.transport.get(url=self.path_urls['files'])
 
@@ -251,13 +276,27 @@ class PathDir(Path):
             path_url = self.path_urls['files'] + path
             path = path[1:]  # strip leading '/'
             if meta['isDirectory']:
-                ret[path] = PathDir(self.transport, path_url)
+                ret[path] = PathDir(self.transport, path_url, path)
             else:
-                ret[path] = PathFile(self.transport, path_url)
+                ret[path] = PathFile(self.transport, path_url, path)
         return ret
 
     def isdir(self):
         return True
+
+    def rename(self, source, target):
+        '''rename file in directory'''
+        json = {'from': source,
+                'to': target,
+                }
+        return self.transport.post(url=self.path_urls['rename'], json=json)
+
+    def copy(self, source, target):
+        '''rename file in directory'''
+        json = {'from': source,
+                'to': target,
+                }
+        return self.transport.post(url=self.path_urls['copy'], json=json)
 
     #def mkdir(self, name):
     #    pass
@@ -265,15 +304,33 @@ class PathDir(Path):
     #    pass
     #def makedirs(self, name):
     #    pass
+    #def remove(self, name):
+    #    '''remove file'''
+    #    pass
 
 
 class PathFile(Path):
-    def __init__(self, transport, path_url):
-        super(PathFile, self).__init__(transport, path_url)
+    def __init__(self, transport, path_url, name):
+        super(PathFile, self).__init__(transport, path_url, name)
 
-    def download(self, max_size=10**6):
-        '''download file into StringIO object, of maximum size `max_size`'''
-        file_ = cStringIO.StringIO()
+    def download(self, file_, max_size=10**6):
+        '''download file
+
+        Args:
+            file_(StringType or file-like): if a string, a file of that name
+            will be created, and filled with the download.  If it's a file-like,
+            then the contents will be write()
+
+            max_size(int): if the file is larger than this, the file won't be
+            downloaded
+
+            >>> import cStringIO
+            >>> foo = wd.listdir()['foo.txt']
+            >>> foo_contents = cStringIO.StringIO()
+            >>> foo.download(foo_contents)
+            >>> print(foo.contents.getvalue())
+        '''
+
         with closing(
             self.transport.get(url=self.path_url,
                                headers={'Accept': 'application/octet-stream'},
@@ -283,8 +340,14 @@ class PathFile(Path):
             resp.raise_for_status()
             if max_size < int(resp.headers['content-length']):
                 raise Exception('File too long')
-            file_.write(resp.content)
-        return file_
+
+            if isinstance(file_, StringType):
+                CHUNK_SIZE = 10 * 1024
+                with open(file_, 'wb') as fd:
+                    for chunk in resp.iter_content(CHUNK_SIZE):
+                        fd.write(chunk)
+            else:
+                file_.write(resp.content)
 
     def isfile(self):
         return True
