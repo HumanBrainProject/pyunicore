eb31a216f53af7b9fb3b49359caa93195898d66c
Bernd Schuller <b.schuller@fz-juelich.de>
2022-12-22 11:08:40 +0100

    inherit from Cluster

--- pyunicore/dask.py
+++ pyunicore/dask.py
@@ -1,13 +1,15 @@
 """ Dask Distributed cluster implementation """
-from multiprocessing import Process
+from multiprocessing import get_context
 
+from distributed.core import rpc
 from distributed.core import Status
+from distributed.deploy.cluster import Cluster
 
 from pyunicore.client import JobStatus
 from pyunicore.forwarder import Forwarder
 
 
-class UNICORECluster:
+class UNICORECluster(Cluster):
     """Deploy Dask on a HPC site via UNICORE"""
 
     def __init__(
@@ -21,6 +23,7 @@ class UNICORECluster:
         local_port=4322,
         debug=False,
     ):
+        super().__init__(asynchronous=asynchronous, name=name, quiet=not debug)
         self.unicore_client = unicore_client
         self.status = Status.created
         self.debug = debug
@@ -28,17 +31,21 @@ class UNICORECluster:
         self.scheduler_options = scheduler_options
         self.worker_options = worker_options
         self.worker_jobs = []
+        self.forwarding_process = None
         try:
             self._start_scheduler()
             self._start_forwarder()
-            self.scheduler_address = "tcp://localhost:%s" % self.local_port
+            self.scheduler_comm = rpc("tcp://localhost:%s" % self.local_port)
+            print("Scheduler address: ", self.scheduler_comm.address)
             if n_workers:
                 self.scale(n_workers)
-        except OSError as e:
+        except OSError:
             try:
                 self.close()
             finally:
-                raise e
+                raise
+        self._loop_runner.start()
+        self.sync(self._start)
 
     def _start_scheduler(self):
         """
@@ -58,10 +65,13 @@ class UNICORECluster:
         }
         job = self.unicore_client.new_job(job_start_scheduler)
         self.scheduler_job = job
-        not self.debug or print("Submitted scheduler job ", job)
+        not self.debug or print("Submitted scheduler", job)
+        not self.debug or print("Waiting for scheduler to start up...")
+
         job.poll(JobStatus.RUNNING)
         if JobStatus.FAILED == job.status:
             raise OSError("Launching scheduler failed")
+        not self.debug or print("Scheduler is running.")
 
     def _start_worker(self):
         """
@@ -73,7 +83,7 @@ class UNICORECluster:
             "Arguments": ["--scheduler-file", "../%s/dask.json" % self.scheduler_job.job_id],
         }
         worker = self.unicore_client.new_job(job_start_worker)
-        not self.debug or print("Submitted worker job ", worker)
+        not self.debug or print("Submitted worker:", worker)
         return worker
 
     def scale(self, n_workers):
@@ -83,6 +93,7 @@ class UNICORECluster:
             self.worker_jobs.append(self._start_worker())
 
     def _start_forwarder(self):
+        not self.debug or print("Starting port forwarder listening on port:", self.local_port)
         endpoint = self.scheduler_job.links["forwarding"]
         tr = self.scheduler_job.transport._clone()
         tr.use_security_sessions = False
@@ -94,7 +105,8 @@ class UNICORECluster:
             login_node=None,
             debug=self.debug,
         )
-        self.forwarding_process = Process(target=self.forwarder.run, args=[self.local_port])
+        ctx = get_context("spawn")
+        self.forwarding_process = ctx.Process(target=self.forwarder.run, args=[self.local_port])
         self.forwarding_process.start()
 
     def close(self):
