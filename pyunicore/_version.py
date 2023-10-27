00f8770746de244815bd8f4467d5787fde963498
Bernd Schuller <b.schuller@fz-juelich.de>
2023-10-27 09:23:20 +0200

    version style

--- pyunicore/_version.py
+++ pyunicore/_version.py
@@ -51,7 +51,7 @@ def get_config() -> VersioneerConfig:
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440-minor"
+    cfg.style = "pep440"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = ""
     cfg.versionfile_source = "pyunicore/_version.py"
